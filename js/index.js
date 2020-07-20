266d39c641cd830f76a64b306277423a28734054
Floydfish <38674510+Floydfish@users.noreply.github.com>
2020-07-21 01:11:56 +0200

    Dynamic page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-<h1>Dificuldade</h1>
+// BOM - window (navegador)
+// DOM - document
+
+// const root = document.querySelector('#root')
+
+// const title = document.createElement('h1')
+
+// title.textContent = 'Dificuldade'
+
+// root.append(title)
+
+// title.innerHTML tem problemas de segurança
+
+const root = document.querySelector('#root')
+
+function newElement(tag, content) {
+    const title = `<${tag}>${content}</${tag}>`
+    root.insertAdjacentHTML('beforeend', title)
+}
+
+newElement('h1', 'Dificuldade')
