677b204db5d444af144e8e0c09bc4d3fdd975c73
Floydfish <38674510+Floydfish@users.noreply.github.com>
2020-07-22 00:04:28 +0200

    reorganizing

--- js/components/Title/index.js
+++ js/components/Title/index.js
@@ -13,9 +13,15 @@
 
 const root = document.querySelector('#root')
 
-function newElement(tag, content) {
-    const title = `<${tag}>${content}</${tag}>`
-    root.insertAdjacentHTML('beforeend', title)
-}
+// function newElement(tag, content) {
+//     const title = `<${tag}>${content}</${tag}>`
+//     root.insertAdjacentHTML('beforeend', title)
+// }
 
-newElement('h1', 'Dificuldade')
+// newElement('h1', 'Eita!')
+
+// Tagged Template String
+
+Title`
+    color: red;
+    `
