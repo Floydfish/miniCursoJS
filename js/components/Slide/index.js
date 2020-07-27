05896d13bc6759c4508333678b65602d304a97a3
Floydfish <38674510+Floydfish@users.noreply.github.com>
2020-07-27 18:51:35 +0200

    Add action icon

--- js/components/Slide/index.js
+++ js/components/Slide/index.js
@@ -1,3 +1,7 @@
+const Action = (css, content = '') => (
+    `<li style="${css}">${content}</li>`
+)
+
 const Item = (css, content = '') => (
     `<li style="${css}">${content}</li>`
 )
@@ -6,12 +10,18 @@ const Slide = (css, content) => (
     `<ul style="${css}">${content}</ul>`
 )
 
+const action = Action`
+    height: calc(var(--line-height)*2);
+    width: calc(var(--line-height)*2);
+    backgroung-color: #00cec9;
+`
+
 const slideContent = 'Vai!'
 
 const item = Item`
     list-style: none;
-    height: calc(var(--line-height)*4);
-    width: calc(var(--line-height)*4);
+    height: calc(var(--line-height)*3);
+    width: calc(var(--line-height)*3);
     background-color: var(--color);
 `
 
